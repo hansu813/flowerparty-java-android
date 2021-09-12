c2b2476155e80c665a674ddb281a9f5ee5004db6
Jonathan <jonnygim.dev@gmail.com>
2021-09-12 22:49:10 +0900

    커뮤니티 채팅리스트 액티비티 이동

--- app/src/main/java/com/example/flowerparty/activity/CommunityChatlistActivity.java
+++ app/src/main/java/com/example/flowerparty/activity/CommunityChatlistActivity.java
@@ -2,15 +2,39 @@ package com.example.flowerparty.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.flowerparty.R;
 
 public class CommunityChatlistActivity extends AppCompatActivity {
 
+    LinearLayout chatWindow;
+    ImageView imgLArrowChatList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community_chatlist);
+
+        chatWindow = findViewById(R.id.chatWindow);
+        chatWindow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CommunityChatActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        imgLArrowChatList = findViewById(R.id.imgLArrowChatList);
+        imgLArrowChatList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/flowerparty/fragment/CommunityFragment.java
+++ app/src/main/java/com/example/flowerparty/fragment/CommunityFragment.java
@@ -1,5 +1,6 @@
 package com.example.flowerparty.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.flowerparty.R;
+import com.example.flowerparty.activity.CommunityChatlistActivity;
+import com.example.flowerparty.activity.ProfileSettingActivity;
 
 public class CommunityFragment extends Fragment {
+
+    ImageView chatList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_community, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_community, container, false);
+
+        chatList = (ImageView) rootView.findViewById(R.id.chatList);
+        chatList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), CommunityChatlistActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return rootView;
     }
 }
