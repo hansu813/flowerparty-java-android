1d48ac44b72f455b409dfd6cc4d4dc5ecf79dc5d
4ayeonk@gmail.com <4ayeonk@gmail.com>
2021-09-12 16:52:47 +0900

    Modify

--- app/src/main/java/com/example/flowerparty/CommunityChatActivity.java
+++ app/src/main/java/com/example/flowerparty/CommunityChatActivity.java
@@ -1,16 +1,14 @@
-package com.example.flowerparty.activity;
+package com.example.flowerparty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.flowerparty.R;
-
-public class HomeActivity extends AppCompatActivity {
+public class CommunityChatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_community_chat);
     }
 }
