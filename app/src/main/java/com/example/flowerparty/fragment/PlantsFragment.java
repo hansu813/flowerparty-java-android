c15cffe948dce574c89c554309fecac777c87169
Jonathan <jonnygim.dev@gmail.com>
2021-09-13 20:51:43 +0900

    plants manage 화면 수정

--- app/src/main/java/com/example/flowerparty/fragment/PlantsFragment.java
+++ app/src/main/java/com/example/flowerparty/fragment/PlantsFragment.java
@@ -1,21 +1,54 @@
 package com.example.flowerparty.fragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.flowerparty.R;
+import com.example.flowerparty.activity.MainActivity;
+import com.example.flowerparty.activity.homeSettingActivity;
 
 public class PlantsFragment extends Fragment {
+    Fragment plantsManageFragment;
+    LinearLayout plantsManage;
+    MainActivity mainActivity;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mainActivity = (MainActivity)getActivity();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mainActivity = null;
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plants, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_plants, container, false);
+
+        plantsManage = (LinearLayout) rootView.findViewById(R.id.plantsDetail);
+        plantsManage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), PlantsManageFragment.class);
+                startActivity(intent);
+            }
+        });
+
+        return rootView;
     }
 }
