8077c9dbbba4e6159b01b990c9928ed223e0b41f
Jonathan <jonnygim.dev@gmail.com>
2021-09-20 10:28:14 +0900

    login

--- app/src/main/java/com/example/flowerparty/RegisterActivity.java
+++ app/src/main/java/com/example/flowerparty/RegisterActivity.java
@@ -1,77 +1,71 @@
-package com.example.flowerparty.activity;
+package com.example.flowerparty;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
-import com.example.flowerparty.R;
-import com.example.flowerparty.RegisterRequest;
+import com.example.flowerparty.activity.LoginActivity;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
-
-    private EditText editTextID, editTextPW, editTextPWC, editTextName, editTextEmail;
+    private EditText et_ID, et_PW, et_cPW, et_Email, et_Name;
     private Button btnRegister;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        // 아이디 값 찾기
-        editTextID = findViewById(R.id.editTextRID);
-        editTextPW = findViewById(R.id.editTextRPW);
-        editTextPWC = findViewById(R.id.editTextPWC);
-        editTextEmail = findViewById(R.id.editTextREmail);
-        editTextName = findViewById(R.id.editTextRName);
+        et_ID = findViewById(R.id.editTextRID);
+        et_PW = findViewById(R.id.editTextRPW);
+        et_cPW = findViewById(R.id.editTextPWC);
+        et_Email = findViewById(R.id.editTextREmail);
+        et_Name = findViewById(R.id.editTextRName);
 
-        // Sign up (회원가입) 버튼 클릭 시 수행
         btnRegister = findViewById(R.id.btnRegister);
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    // EditText 에 현재 입력된 값 가져오기
-                String userID = editTextID.getText().toString();
-                String userPass = editTextPW.getText().toString();
-                String userName = editTextName.getText().toString();
-                String userEmail = editTextEmail.getText().toString();
-//
+                String userID = et_ID.getText().toString();
+                String userPassword = et_PW.getText().toString();
+                String userEmail = et_Email.getText().toString();
+                String userName = et_Name.getText().toString();
+
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
                             JSONObject jsonObject = new JSONObject(response);
                             boolean success = jsonObject.getBoolean("success");
-                            if (success) { // 회원 등록 성공
+                            if (success) {
                                 Toast.makeText(getApplicationContext(), "회원 등록에 성공하였습니다.", Toast.LENGTH_SHORT).show();
                                 Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                                 startActivity(intent);
-                            } else { // 회원등록 실패
+                            } else {
                                 Toast.makeText(getApplicationContext(), "회원 등록에 실패하였습니다.", Toast.LENGTH_SHORT).show();
-                                return;
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
+                            Log.e("Error", "Response Error", e);
                         }
                     }
                 };
-                // 서버로 Volley 를 이용해서 요청을 함.
-                RegisterRequest registerRequest = new RegisterRequest(userID, userPass, userName, userEmail, responseListener);
+                RegisterRequest registerRequest = new RegisterRequest(userID, userPassword, userEmail, userName, responseListener);
                 RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
                 queue.add(registerRequest);
             }
         });
+
     }
 }
--- app/src/main/java/com/example/flowerparty/activity/LoginActivity.java
+++ app/src/main/java/com/example/flowerparty/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,11 +15,10 @@ import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
 import com.example.flowerparty.LoginRequest;
 import com.example.flowerparty.R;
-import com.example.flowerparty.RegisterActivity2;
+import com.example.flowerparty.RegisterActivity;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -35,57 +35,56 @@ public class LoginActivity extends AppCompatActivity {
         btnSignIn = findViewById(R.id.btnSignIn);
         btnSignUp = findViewById(R.id.btnSignUp);
 
-        // Sign up (회원가입) 버튼 클릭시 수행
+        // By.Jongwon  Sign up (회원가입) 버튼 클릭시 RegisterActivity로 이동
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity2.class);
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
 
         // Sign In (로그인) 버튼 클릭시 수행
-//        btnSignIn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // editText 에 입력되어 있는 값 가져오기
-//                String userID = editTextID.getText().toString();
-//                String userPass = editTextPW.getText().toString();
-//
-//                Response.Listener<String> responseListener = new Response.Listener<String>() {
-//                    @Override
-//                    public void onResponse(String response) {
-//                        try {
-//                            // TODO : 인코딩 문제 때문에 한글 DB 인 경우 로그인 불가
-//                            System.out.println("hongchul" + response);
-//                            JSONObject jsonObject = new JSONObject(response);
-//                            boolean success = jsonObject.getBoolean("success");
-//                            if (success) { // 로그인 성공
-//                                String userID = jsonObject.getString("userID");
-//                                String userPass = jsonObject.getString("userPassword");
-//
-//                                Toast.makeText(getApplicationContext(), "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
-//                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//                                intent.putExtra("userID", userID);
-//                                intent.putExtra("userPass", userPass);
-//                                startActivity(intent);
-//                            } else { // 로그인 실패
-//                                Toast.makeText(getApplicationContext(), "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show();
-//                                return;
-//                            }
-//                    } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                };
-//                LoginRequest loginRequest = new LoginRequest(userID, userPass, responseListener);
-//                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
-//                queue.add(loginRequest);
-//            }
-//        });
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // editText 에 입력되어 있는 값 가져오기
+                String userID = editTextID.getText().toString();
+                String userPass = editTextPW.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success) { // 로그인 성공
+                                String userID = jsonObject.getString("userID");
+                                String userPass = jsonObject.getString("userPassword");
+
+                                Toast.makeText(getApplicationContext(), "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                //intent.putExtra("userID", userID);
+                                //intent.putExtra("userPass", userPass);
+                                startActivity(intent);
+                            } else { // 로그인 실패
+                                Toast.makeText(getApplicationContext(), "아이디와 비밀번호를 확인하세요.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                    } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.e("Error", "Response Error", e);
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(userID, userPass, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+            }
+        });
 
 
-
+        // 로그인 구현 전 액티비티 이동만 구현
 //        Button btnChk = findViewById(R.id.btnSignIn);
 //
 //        btnChk.setOnClickListener(new View.OnClickListener() {
