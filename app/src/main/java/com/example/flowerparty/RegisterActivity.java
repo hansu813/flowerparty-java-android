533fc7d1dbc7e6aa109ca70c44f19049eccfa64e
Jonathan <jonnygim.dev@gmail.com>
2021-09-21 12:23:31 +0900

    아이디 중복체크

--- app/src/main/java/com/example/flowerparty/RegisterActivity.java
+++ app/src/main/java/com/example/flowerparty/RegisterActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -21,8 +24,29 @@ import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText et_ID, et_PW, et_cPW, et_Email, et_Name;
+    private TextView textValidate;
     private Button btnRegister;
     private AlertDialog dialog;
+    private boolean validate = false;
+
+
+    private final TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            //textValidate.setBackgroundColor(getResources().getColor(R.color.gray_green));
+            return;
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +58,58 @@ public class RegisterActivity extends AppCompatActivity {
         et_cPW = findViewById(R.id.editTextPWC);
         et_Email = findViewById(R.id.editTextREmail);
         et_Name = findViewById(R.id.editTextRName);
+        textValidate = findViewById(R.id.textValidate);
 
+
+        // 아이디 중복 체크
+        textValidate = findViewById(R.id.textValidate);
+        textValidate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userID = et_ID.getText().toString();
+                if (validate) {
+                    return; // 중복 확인 완료
+                }
+                if (et_ID.equals("")) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    dialog = builder.setMessage("아이디를 입력하세요.").setPositiveButton("확인", null).create();
+                    dialog.show();
+                    return;
+                }
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+
+                            AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                            if (success) {
+                                dialog = builder.setMessage("사용할 수 있는 아이디입니다.").setPositiveButton("확인", null).create();
+                                dialog.show();
+                                //et_ID.setEnabled(false); //아이디값 고정
+                                validate = true; //검증 완료
+                                textValidate.setBackgroundColor(getResources().getColor(R.color.white));
+                            } else {
+                                dialog = builder.setMessage("이미 존재하는 아이디입니다.").setNegativeButton("확인", null).create();
+                                dialog.show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.e("Error", "Response Error", e);
+                        }
+                    }
+                };
+                ValidateRequest validateRequest = new ValidateRequest(userID, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(validateRequest);
+            }
+        });
+
+
+
+        // 회원 가입 클릭시 수행
         btnRegister = findViewById(R.id.btnRegister);
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,7 +127,7 @@ public class RegisterActivity extends AppCompatActivity {
                             JSONObject jsonObject = new JSONObject(response);
                             boolean success = jsonObject.getBoolean("success");
 
-                            if (userPassword.equals(userCPassword)) {
+                            if (userPassword.equals(userCPassword) && !userName.equals("") && !userEmail.equals("")) {
                                 if (success) { // 회원가입 성공
                                     Toast.makeText(getApplicationContext(), "회원 등록에 성공하였습니다.", Toast.LENGTH_SHORT).show();
                                     Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
@@ -60,9 +135,14 @@ public class RegisterActivity extends AppCompatActivity {
                                 } else {
                                     Toast.makeText(getApplicationContext(), "회원 등록에 실패하였습니다.", Toast.LENGTH_SHORT).show();
                                 }
-                            } else {
+                            } else if (!userPassword.equals(userCPassword)){
                                 AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
                                 dialog = builder.setMessage("비밀번호가 동일하지 않습니다.").setNegativeButton("확인", null).create();
+                                dialog.show();
+                                return;
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                dialog = builder.setMessage("항목을 모두 입력하세요.").setPositiveButton("확인", null).create();
                                 dialog.show();
                                 return;
                             }
@@ -77,6 +157,9 @@ public class RegisterActivity extends AppCompatActivity {
                 queue.add(registerRequest);
             }
         });
+
+
+
 
     }
 }
