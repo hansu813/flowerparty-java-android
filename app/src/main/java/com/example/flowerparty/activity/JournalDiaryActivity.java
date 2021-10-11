de75726af619da094e62f00867f2693f861a22e6
Jonathan <jonnygim.dev@gmail.com>
2021-10-11 22:47:27 +0900

    journal & journal diary 페이지 이동 (nayeane)

--- app/src/main/java/com/example/flowerparty/activity/JournalDiaryActivity.java
+++ app/src/main/java/com/example/flowerparty/activity/JournalDiaryActivity.java
@@ -6,13 +6,35 @@ import android.os.Bundle;
 
 import com.example.flowerparty.R;
 
+import android.content.Intent;
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.flowerparty.R;
+import com.example.flowerparty.activity.MainActivity;
+import com.example.flowerparty.fragment.JournalFragment;
+
+
+
 public class JournalDiaryActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_diary);
-    }
 
+
+    }
 
 }
