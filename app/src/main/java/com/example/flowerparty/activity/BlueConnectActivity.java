e9ebf7b9f03c2ad8d47a44a8908f34cbb880f4e7
Jonathan <jonnygim.dev@gmail.com>
2021-10-11 16:00:13 +0900

    Revert "블루투스 관련 수정"
    This reverts commit 5e870186fb7680cbd0f5ae9dd5aa49a8df2f025e.

--- app/src/main/java/com/example/flowerparty/activity/BlueConnectActivity.java
+++ app/src/main/java/com/example/flowerparty/activity/BlueConnectActivity.java
@@ -1,78 +1,26 @@
  package com.example.flowerparty.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import android.Manifest;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.Html;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.Switch;
-import android.widget.TextView;
 
 import com.example.flowerparty.R;
-
-import java.util.ArrayList;
-import java.util.Set;
 
  public class BlueConnectActivity extends AppCompatActivity {
      ImageView imgLArrowBlue;
      Switch switchBlue;
 
-     BluetoothAdapter btAdapter;
-     private final static int REQUEST_ENABLE_BT = 1;
-
-     Set<BluetoothDevice> pairedDevices;
-     ArrayAdapter<String> btArrayAdapter;
-     ArrayList<String> deviceAddressArray;
-     TextView textStatus;
-     Button btnParied, btnSearch, btnSend;
-     ListView listView;
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blue_connect);
-
-        // Get permission
-        String[] permission_list = {
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-        };
-        ActivityCompat.requestPermissions(BlueConnectActivity.this, permission_list,  1);
-
-        // Enable bluetooth
-        btAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (!btAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-
-        // variables
-        textStatus = (TextView) findViewById(R.id.textViewStatus);
-        btnParied = (Button) findViewById(R.id.btnPaired);
-        btnSearch = (Button) findViewById(R.id.btnSearch);
-        btnSend = (Button) findViewById(R.id.btnSend);
-        listView = (ListView) findViewById(R.id.listviewBT);
-
-        // show paired devices
-        btArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
-        deviceAddressArray = new ArrayList<>();
-        listView.setAdapter(btArrayAdapter);
-
-
 
         imgLArrowBlue = findViewById(R.id.imgLArrowBlue);
         imgLArrowBlue.setOnClickListener(new View.OnClickListener() {
@@ -84,31 +32,16 @@ import java.util.Set;
 
         // 블루투스 on, off 스위치
         switchBlue = findViewById(R.id.switchBlue);
-  //      switchBlue.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            LinearLayout blueOn = (LinearLayout) findViewById(R.id.blueOn);
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                if (isChecked) {
-//                    blueOn.setVisibility(View.VISIBLE);
-//                } else {
-//                    blueOn.setVisibility(View.INVISIBLE);
-//                }
-//            }
-//        });
+        switchBlue.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            LinearLayout blueOn = (LinearLayout) findViewById(R.id.blueOn);
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    blueOn.setVisibility(View.VISIBLE);
+                } else {
+                    blueOn.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
-
-     public void onClickButtonPaired(View view){
-         btArrayAdapter.clear();
-         if(deviceAddressArray!=null && !deviceAddressArray.isEmpty()){ deviceAddressArray.clear(); }
-         pairedDevices = btAdapter.getBondedDevices();
-         if (pairedDevices.size() > 0) {
-             // There are paired devices. Get the name and address of each paired device.
-             for (BluetoothDevice device : pairedDevices) {
-                 String deviceName = device.getName();
-                 String deviceHardwareAddress = device.getAddress(); // MAC address
-                 btArrayAdapter.add(deviceName);
-                 deviceAddressArray.add(deviceHardwareAddress);
-             }
-         }
-     }
 }
--- app/src/main/java/com/example/flowerparty/activity/MainActivity.java
+++ app/src/main/java/com/example/flowerparty/activity/MainActivity.java
@@ -2,14 +2,8 @@ package com.example.flowerparty.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-import android.Manifest;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -20,8 +14,6 @@ import com.example.flowerparty.fragment.PlantsFragment;
 import com.example.flowerparty.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.io.OutputStream;
-
 public class MainActivity extends AppCompatActivity {
     Fragment homeFragment;
     Fragment plantsFragment;
@@ -29,18 +21,10 @@ public class MainActivity extends AppCompatActivity {
     Fragment MypageAFragment;
 
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
-
 
         homeFragment = new HomeFragment();
         plantsFragment = new PlantsFragment();
