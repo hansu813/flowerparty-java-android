a26ac481d9d38f995f2f7a44d09d06d43ec40425
Jonathan <jonnygim.dev@gmail.com>
2021-10-12 18:44:49 +0900

    식물선택창 UI 수정

--- app/src/main/java/com/example/flowerparty/activity/PlantsChooseActivity.java
+++ app/src/main/java/com/example/flowerparty/activity/PlantsChooseActivity.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
